9cd73f1a123c8343b1495c089d2073bd1278868b
luizffdemoraes <lffm1994@gmail.com>
2021-02-04 22:20:22 -0300

    correção Util

--- aula3/scripts.js
+++ aula3/scripts.js
@@ -142,7 +142,7 @@ const Utils = {
   formatAmount(value) {
     value = Number(value) * 100;
     //value = Number(value.replace(/\,\./g, "")) * 100; Outra opção Maikão
-    return value;
+    return Math.round(value);
   },
 
   formatDate(date) {
